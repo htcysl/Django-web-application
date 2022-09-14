b4a40c5688925a5ae2ec10d4e15c7e8704f9d352
Hatice Uysal <maahhaticeuysal@mail.com>
2022-09-14 12:19:09 +0300

    changed update view

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -28,19 +28,19 @@ def delete(request,id): # uses the id to locate the correct record in the Member
     member.delete()
     return HttpResponseRedirect(reverse('index'))
 
-def update(request,id):
-    mymember = Members.objects.get(id=id)
-    template = loader.get_template('update.html')
-    context ={
-        'mymember': mymember,
-    }
-    return HttpResponse(template.render(context, request))
+def update(request, id):
+  mymember = Members.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
 
-def updaterecord(request,id):
-    first = request.POST['first']
-    last = request.POST['last']
-    member = Members.objects.get(id=id)
-    member.firstname = first
-    member.lastname = last
-    member.save()
-    return HttpResponseRedirect(reverse('index'))    
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Members.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
+  member.save()
+  return HttpResponseRedirect(reverse('index')) 
